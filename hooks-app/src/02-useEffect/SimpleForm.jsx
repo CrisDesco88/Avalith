64cff37b48a55178b7e726a010bcc0b093c8d834
Usuario <cristiandescosido@gmail.com>
2022-07-24 19:55:55 -0300

    Corregido renderizado del <hr/>

---
+++ hooks-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+  });
+
+  const { username, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return (
+    <>
+      <h1>Simple Form</h1>
+      <hr />
+
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+      <input
+        type="email"
+        className="form-control"
+        placeholder="youremail@gmail.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
--- hooks-app/src/App.jsx
+++ hooks-app/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
-
+// import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+import { SimpleForm } from "./02-useEffect/SimpleForm";
+import "./App.css"
 function App() {
   return (
-    <div>
+    <div className="d-flex flex-column w-50 my-0 mx-auto justify-content-center vh-100">
       <h1>Hooks App</h1>
       <hr />
-      <CounterWithCustomHook />
+      <SimpleForm />
     </div>
   );
 }
